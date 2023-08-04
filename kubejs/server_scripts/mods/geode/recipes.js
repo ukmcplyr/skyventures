5a5758e91eda3b87878f74078023f11038db89c2
Francis M <ukmcplyr@gmail.com>
2023-08-04 19:25:42 +0000

    kubejs/server_scripts/mods/geode/recipes.js: Add redstone, emerald, diamond, quartz and glowstone

--- kubejs/server_scripts/mods/geode/recipes.js
+++ kubejs/server_scripts/mods/geode/recipes.js
@@ -2,11 +2,19 @@
 
 ServerEvents.recipes(event =>
 {
-  const buds = [
+  const overworld_buds = [
     'lapis',
+    'redstone',
+    'emerald',
+    'diamond',
   ]
 
-  buds.forEach(bud =>
+  const nether_buds = [
+    'quartz',
+    'glowstone',
+  ]
+
+  overworld_buds.forEach(bud =>
   {
     event.shaped(
       `geode:budding_${bud}`,
@@ -24,15 +32,58 @@ ServerEvents.recipes(event =>
     event.shaped(
       `geode:budding_deepslate_${bud}`,
       [
-        'DDD',
-        'DBD',
-        'DDD',
+        'BBB',
+        'BAB',
+        'BBB',
       ],
       {
-        D: 'minecraft:deepslate',
-        B: `geode:budding_${bud}`
+        B: 'minecraft:deepslate',
+        A: `geode:budding_${bud}`
       }
     )
+  })
+
+  nether_buds.forEach(bud =>
+  {
+    event.shaped(
+      `geode:budding_nether_${bud}`,
+      [
+        'BBB',
+        'BAB',
+        'BBB',
+      ],
+      {
+        B: `geode:large_nether_${bud}_bud`,
+        A: 'minecraft:budding_amethyst',
+      }
+    )
+
+    event.shaped(
+      `geode:budding_basalt_${bud}`,
+      [
+        'BBB',
+        'BAB',
+        'BBB',
+      ],
+      {
+        B: 'minecraft:basalt',
+        A: `geode:budding_nether_${bud}`,
+      }
+    )
+
+    event.shaped(
+      `geode:budding_blackstone_${bud}`,
+      [
+        'BBB',
+        'BAB',
+        'BBB',
+      ],
+      {
+        B: 'minecraft:blackstone',
+        A: `geode:budding_basalt_${bud}`,
+      }
+    )
+
   })
 
 })
