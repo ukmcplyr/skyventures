6d9d0699c71d9c57fb401ef0224fbda2c13cc2b9
Francis M <ukmcplyr@gmail.com>
2023-08-14 02:13:23 +0000

    kubejs/server_scripts/mods/botania/recipes.js: Remove geode bud recipes

--- kubejs/server_scripts/mods/botania/recipes.js
+++ kubejs/server_scripts/mods/botania/recipes.js
@@ -37,51 +37,6 @@ ServerEvents.recipes(event =>
     500
   ))
 
-  event.remove({id: 'botania:mana_infusion/quartz_deconstruct'})
-  event.remove({id: 'botania:mana_infusion/glowstone_deconstruct'})
-
-  event.custom(BotaniaManaInfusedAlchemyRecipe(
-    ALCHEMY_CATALYST,
-    'minecraft:lapis_block',
-    'geode:large_lapis_bud',
-    6250
-  ))
-
-  event.custom(BotaniaManaInfusedAlchemyRecipe(
-    ALCHEMY_CATALYST,
-    'minecraft:redstone_block',
-    'geode:large_redstone_bud',
-    6250
-  ))
-
-  event.custom(BotaniaManaInfusedAlchemyRecipe(
-    ALCHEMY_CATALYST,
-    'minecraft:emerald_block',
-    'geode:large_emerald_bud',
-    6250
-  ))
-
-  event.custom(BotaniaManaInfusedAlchemyRecipe(
-    ALCHEMY_CATALYST,
-    'minecraft:diamond_block',
-    'geode:large_diamond_bud',
-    6250
-  ))
-
-  event.custom(BotaniaManaInfusedAlchemyRecipe(
-    ALCHEMY_CATALYST,
-    'minecraft:quartz_block',
-    'geode:large_nether_quartz_bud',
-    6250
-  ))
-
-  event.custom(BotaniaManaInfusedAlchemyRecipe(
-    ALCHEMY_CATALYST,
-    'minecraft:glowstone',
-    'geode:large_nether_glowstone_bud',
-    6250
-  ))
-
 })
 
 //
